4226096d4a2e308489d851258b39c1df3d2c64ff
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-09-26 10:47:43 +0100

    ok

--- fix_venue.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Simple replacement script for venue references
-sed -i 's/{{ $booking->venue }}/{{ $booking->venue->venue_name }}/g' "/c/Laravel_new/bnb/resources/views/livewire/bookings-table.blade.php"
