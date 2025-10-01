a1a311b2caba85feadad2967d36f4fccb9f6055e
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-10-01 10:43:53 +0100

    ok

--- routes/console.php
+++ routes/console.php
@@ -21,6 +21,7 @@ Artisan::command('test:reviews', function () {
 })->purpose('Run review emails manually');
 
 // Schedule the email jobs to run daily
+Schedule::job(new SendCheckinReminders())->dailyAt('10:45')->name('send-checkin-reminders');
 Schedule::job(new SendReviewLinkEmails())->dailyAt('09:00')->name('send-review-link-emails');
 
 // Schedule cleanup of abandoned bookings - runs every hour
