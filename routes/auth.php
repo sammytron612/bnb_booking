b0bc7212ddb8a4d71f65b91c9d7529e1bf03819c
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-10-04 01:54:11 +0100

    ok

--- routes/auth.php
+++ routes/auth.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::middleware('guest')->group(function () {
-    Volt::route('danya', 'auth.login')
+    Volt::route('danya-login', 'auth.login')
         ->name('login');
 
   //  Volt::route('register', 'auth.register')
