2ff35c60c37d0104a8f4c4f75785cff358e05310
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2025-10-16 15:33:42 +0100

    ok

--- resources/views/livewire/admin/manual-booking.blade.php
+++ resources/views/livewire/admin/manual-booking.blade.php
@@ -222,7 +222,7 @@
                         <button
                             type="button"
                             wire:click="closeModal"
-                            class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                            class="my-2 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                             Cancel
                         </button>
                     </div>
